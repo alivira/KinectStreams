713f38d0947648d77cc57e891f085d108e749066
-- Charis Chao <-- Charis Chao>
2017-03-26 22:05:00 -0400

    Adding actual history pages

---
+++ History.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows.Forms;
+
+namespace KinectStreams
+{
+    /// <summary>
+    /// Interaction logic for History.xaml
+    /// </summary>
+    public partial class History : Window
+    {
+        public History()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            string[] files = Directory.GetFiles(Environment.CurrentDirectory);
+            foreach(string file in files)
+            {
+                if (file.Contains("EntireScreenCapture"))
+                {
+                    listVideo.Items.Add(file);
+                }
+            }
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
