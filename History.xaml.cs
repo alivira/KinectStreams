604c70149bc5d0063d77c473e3a2dfeb0b22c45a
-- Charis Chao <-- Charis Chao>
2017-03-26 22:46:51 -0400

    Adding media player to history page

--- History.xaml.cs
+++ History.xaml.cs
@@ -28,19 +28,45 @@ namespace KinectStreams
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            for(int i = listVideo.Items.Count - 1; i >= 0; i--)
+            {
+                listVideo.Items.RemoveAt(i);
+            }
             string[] files = Directory.GetFiles(Environment.CurrentDirectory);
             foreach(string file in files)
             {
                 if (file.Contains("EntireScreenCapture"))
                 {
-                    listVideo.Items.Add(file);
+                    
+                    string temp = file;
+                    temp = temp.Remove(0, file.IndexOf("EntireScreenCapture"));
+                    listVideo.Items.Add(temp);
                 }
             }
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
+            
+        }
+
+        private void btnMenu_Click(object sender, RoutedEventArgs e)
+        {
             this.Hide();
+        }
+
+        private void btnPlay_Click(object sender, RoutedEventArgs e)
+        {
+            if(listVideo.SelectedItems.Count == 0)
+            {
+                System.Windows.MessageBox.Show("Please select a video to play.", "Invalid video file", MessageBoxButton.OK);
+            }
+            string video = listVideo.SelectedItem.ToString();
+            video = Environment.CurrentDirectory + "//" + video;
+            txtTip.Text = "Play for longer";
+            mediaplayer.Source = new Uri(video);
+            mediaplayer.UnloadedBehavior = MediaState.Manual;
+            mediaplayer.Play();
         }
     }
 }
